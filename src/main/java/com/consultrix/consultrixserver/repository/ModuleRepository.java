6918e2583ec41d5134b5cb91abf321ec4800b243
DoughSec <addisondoseck@gmail.com>
2026-02-18 09:37:06 -0500

    added module repo

---
+++ src/main/java/com/consultrix/consultrixserver/repository/ModuleRepository.java
@@ -0,0 +1,9 @@
+package com.consultrix.consultrixserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+    List<Module> findByCohortIdOrderByOrderIndexAsc(int cohortId);
+}
