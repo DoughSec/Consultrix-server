aa9d83aac37f7537991bacbb3e004d3ff6247193
DoughSec <addisondoseck@gmail.com>
2026-02-19 09:39:56 -0500

    added CRUD to organization service

--- src/main/java/com/consultrix/consultrixserver/service/OrganizationService.java
+++ src/main/java/com/consultrix/consultrixserver/service/OrganizationService.java
@@ -5,6 +5,8 @@ import com.consultrix.consultrixserver.repository.OrganizationRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class OrganizationService {
@@ -15,12 +17,37 @@ public class OrganizationService {
     }
 
     //create
-//    public Organization createOrganiztion(Organization)
+    public Organization create(Organization org) {
+        if(organizationRepository.findByNameIgnoreCase(org.getName())) {
+            throw new IllegalArgumentException("Organization already exists");
+        }
+        return organizationRepository.save(org);
+    }
     //getById
+    public Organization getById(Integer id) {
+        return organizationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Organization not found")); //lamba function for exception
+    }
 
     //getAll
-
+    public List<Organization> getAll() {
+        return organizationRepository.findAll();
+    }
     //update
+    public Organization update(Integer id, Organization updated) {
+        //get the already existing org
+        Organization existingOrg = getById(id);
+
+        //set the things we want to update
+        existingOrg.setName(updated.getName());
+        existingOrg.setType(updated.getType());
+        existingOrg.setContactEmail(updated.getContactEmail());
+
+        return organizationRepository.save(existingOrg);
+    }
 
     //delete
+    public void delete(Integer id) {
+        organizationRepository.deleteById(id);
+    }
 }
