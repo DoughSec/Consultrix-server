12f140c3302b628e9d303ae4401aea444673e731
DoughSec <addisondoseck@gmail.com>
2026-02-20 10:53:25 -0500

    added instructor controller, still need to check post mapping

---
+++ src/main/java/com/consultrix/consultrixserver/controller/InstructorController.java
@@ -0,0 +1,54 @@
+package com.consultrix.consultrixserver.controller;
+
+import com.consultrix.consultrixserver.model.Instructor;
+import com.consultrix.consultrixserver.service.InstructorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consultrix/Instructor")
+public class InstructorController {
+    private final InstructorService instructorService;
+
+    public InstructorController(InstructorService instructorService) {
+        this.instructorService = instructorService;
+    }
+
+    //create Instructor record
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Instructor create(@RequestBody Instructor request) {
+        return instructorService.create(request);
+    }
+
+    //get all Instructor records
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Instructor> getAll() {
+        return instructorService.getAll();
+    }
+
+    //get Instructor by id
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Instructor getInstructorById(@PathVariable("id") Integer id) {
+        return instructorService.getById(id);
+    }
+
+    //update Instructor record
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Instructor updateInstructor(@PathVariable("id") Integer id, @RequestBody Instructor instructor) {
+        return instructorService.update(id, instructor);
+    }
+
+    //delete Instructor record
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteInstructor(@PathVariable("id") Integer id) {
+        instructorService.delete(id);
+    }
+
+}
